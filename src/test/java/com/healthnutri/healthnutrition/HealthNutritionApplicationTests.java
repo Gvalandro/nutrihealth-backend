9c788a547084055fea95e4c5e05e12c449dac3c3
Gustavo Valandro <72421101+Gvalandro@users.noreply.github.com>
2024-07-30 17:45:26 +0100

    Creating Project

---
+++ src/test/java/com/healthnutri/healthnutrition/HealthNutritionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthnutri.healthnutrition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthNutritionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
